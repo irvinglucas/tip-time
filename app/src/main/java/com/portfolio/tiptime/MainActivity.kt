4d045277139661994e5d9bd9397e74da50046eda
Irving Lucas da Silva Melo <irving.lucas@gmail.com>
2022-08-30 16:40:41 -0300

    First commit with a lot of things. I think the app is almost done, but i'll create test and refactor the screen, i'll add some more funcionalities too. That's it for today.

---
+++ app/src/main/java/com/portfolio/tiptime/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.portfolio.tiptime
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.portfolio.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    // this code is using view binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            if (binding.costOfServiceField.text.isEmpty()) {
+                val text = getString(R.string.empty_input_text_warning)
+                Toast.makeText(this, text, Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                calculateTip()
+            }
+        }
+    }
+
+    private fun calculateTip() {
+
+        /*
+        * the "binding.costOfServiceField.text" property is an 'Editable' object, so it can't be converted to double. To do so, you need to convert it to string first and then convert to double. */
+        val stringInTextField = binding.costOfServiceField.text.toString()
+
+        // here is the bill, converted to double
+        val cost = stringInTextField.toDouble()
+
+        /* I concluded that when the user changes the option, the checkedRadioButtonId number changes. So you just have to check this ID and do whatever you need to do.*/
+
+        /* here we get the percentage and stores it in a variable with the a converted value that i'll ease the calculations*/
+        val tipPercentage = when (binding.tipsOptionsGroup.checkedRadioButtonId) {
+            R.id.tip_option_one -> 0.20
+            R.id.tip_option_two -> 0.18
+            R.id.tip_option_three -> 0.15
+            else -> 0.0
+        }
+
+        // calculating the percentage
+        val tip = cost * tipPercentage
+
+        // calculating the total cost of the service. Bill  + tip on the bill
+        var finalCostWithTip = when (binding.roundTipSwitch.isChecked) {
+            true -> {
+                cost + ceil(tip)
+            }
+            else -> {
+                cost + tip
+            }
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        val formattedTotalCost = NumberFormat.getCurrencyInstance().format(finalCostWithTip)
+
+        binding.tipResult.text = getString(R.string.formatted_tip ,formattedTip)
+        binding.totalCostWithTip.text = getString(R.string.formatted_total_cost ,formattedTotalCost)
+    }
+}
